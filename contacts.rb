4c50e1de434b1d7766fe7e72829fc9668260a058
Kevin Diesenberg <kdiesenb@gmail.com>
2022-12-12 19:40:12 -0500

    this fails even tho its correct

--- contacts.rb
+++ contacts.rb
@@ -17,10 +17,28 @@ def contacts
   }
 end
 
+# def remove_strawberry(contacts)
+#   # your code here!
+#   contacts.each do |person, contact_details_hash|
+#     if person == "Freddie Mercury"
+#       contact_details_hash.each do |attribute, data|
+#         if attribute == :favorite_ice_cream_flavors
+#           data.delete_if { |ice_cream| ice_cream == "strawberry" }
+#         end
+#       end
+#     end
+#   end
+# end
+
 def remove_strawberry(contacts)
-  # your code here!
-  contacts.each do [person, contact_details_hash]
-  binding.pry
+  contacts.each do |person, contact_details_hash|
+    if person == "Freddie Mercury"
+      contact_details_hash.each do |attribute, data|
+        if attribute == :favorite_ice_cream_flavors
+          data.delete_if {|ice_cream| ice_cream == "strawberry"}
+        end
+      end
+    end
   end
 end
 
